d077995869b5394b01f6d791d88553b15c31ccb8
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2020-01-20 14:02:23 -0500

    Added link to other cross reference in the gene view

--- exphewas/backend/backend.py
+++ exphewas/backend/backend.py
@@ -2,12 +2,22 @@
 Flask-based application for ExPheWAS.
 """
 
+from collections import defaultdict
+
 from flask import Blueprint, render_template, abort
 
 from . import api
 
 
 backend = Blueprint("backend_blueprint", __name__)
+
+EXTERNAL_DB_URL = {
+    "WikiGene": "https://www.wikigenes.org/e/gene/e/{id}.html",
+    "MIM_GENE": "https://omim.org/entry/{id}",
+    "MIM_MORBID": "https://omim.org/entry/{id}",
+}
+
+EXTERNAL_DB_TO_SHOW = ("WikiGene", "MIM_GENE", "MIM_MORBID")
 
 
 @backend.route("/outcome")
@@ -35,4 +45,14 @@ def get_gene(ensg):
         gene_info = api.get_gene_by_ensembl_id(ensg)
     except api.RessourceNotFoundError as exception:
         abort(404)
-    return render_template("gene.html", **gene_info)
+
+    # Adding the cross references
+    xrefs = defaultdict(list)
+    db_names = {}
+    for xref in api.get_gene_xrefs(ensg):
+        db_names[xref["db_id"]] = xref["db_description"]
+        xrefs[xref["db_id"]].append(xref["external_id"])
+
+    return render_template("gene.html", **gene_info, xrefs=xrefs,
+                           db_names=db_names, db_urls=EXTERNAL_DB_URL,
+                           external_dbs=EXTERNAL_DB_TO_SHOW)
