1b8270123be732d0b29ec84fa835181fab355573
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2022-02-21 14:29:43 -0500

    Fixed the curl command (quoting the URL)

--- frontend/src/documentation.js
+++ frontend/src/documentation.js
@@ -52,10 +52,10 @@ async function modalCallback(event) {
     // Populate the modal example.
     let commandText;
     if (method == 'GET') {
-      commandText = `curl https://${window.location.host}${reqURL}`;
+      commandText = `curl --silent 'https://${window.location.host}${reqURL}'`;
     } else {
       // TODO
-      commandText = `curl -X ${method} https://${window.location.host}${reqURL}`;
+      commandText = `curl --silent -X ${method} 'https://${window.location.host}${reqURL}'`;
     }
     modal.find('.example-command').text(commandText);
 
